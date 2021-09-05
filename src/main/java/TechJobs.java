d8edbb33fbd8950320dc525a7d399f4fa2315d2d
Vetri Kaliyamurthy <VKaliyamurthy@tvsscs.com>
2021-09-05 08:44:44 -0500

    Assignment done

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -43,15 +43,12 @@ public class TechJobs {
                 } else {
 
                     ArrayList<String> results = JobData.findAll(columnChoice);
-
-                    System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
-
+                    System.out.println("\n*** All " + columnChoices.get(columnChoice) + "Values ***");
                     // Print list of skills, employers, etc
                     for (String item : results) {
                         System.out.println(item);
                     }
                 }
-
             } else { // choice is "search"
 
                 // How does the user want to search (e.g. by skill or employer)
@@ -119,7 +116,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> aJob : someJobs) {
+                System.out.println("\n*****");
+                for (String jobKey : aJob.keySet()) {
+                    System.out.println(jobKey + ": " + aJob.get(jobKey));
+                }
+                System.out.println("*****");
+            }
 
-        System.out.println("printJobs is not implemented yet");
+        }
     }
 }
